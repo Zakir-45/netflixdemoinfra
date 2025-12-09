ea0199470fdc59d368c5c79e18eaf6609f9f7896
Zakir-45 <jsk41401@gmail.com>
2025-12-09 12:39:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1b"
 }
 
 resource "aws_instance" "one" {
   count                  = 4
-  ami                    = "ami-0360c520857e3138f"
+  ami                    = "ami-00ca570c1b6d79f36 "
   instance_type          = "t2.medium"
-  key_name               = "Netflix"
+  key_name               = "heena"
   vpc_security_group_ids = ["sg-0ead21b7862e54f9a"]
   tags = {
     Name = var.instance_names[count.index]
